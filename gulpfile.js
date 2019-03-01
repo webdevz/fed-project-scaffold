da22eede40a09d9a4daa991327cf02a9a6d84f67
Butterfield <dave.butterfield@wwwinc.com>
2019-03-01 15:40:42 -0500

    FED Project Scaffold

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,16 @@
 const gulp = require('gulp');
 
 // Gulp helpers
-const insert = require('gulp-insert');
-const sequence = require('gulp-sequence');
-const glob = require('glob');
-const es = require('event-stream');
+//const insert = require('gulp-insert');
+//const sequence = require('gulp-sequence');
+//const glob = require('glob');
+//const es = require('event-stream');
 const del = require('del');
 const injectCSS = require('gulp-inject-css');
 const injectJS = require('gulp-inject-js');
+const webp = require('gulp-webp');
+const imagemin = require('imagemin');
+const imageminWebp = require('imagemin-webp');
 
 // Define relevant paths
 const paths = {
@@ -16,49 +19,49 @@ const paths = {
 		html: './src/**/*.html',
 		scripts: './src/**/*.js',
 		stylesheets: ['./src/**/*.css', './src/**/*.scss', './src/**/*.sass'],
+		images: ['./src/images/*.jpg', './src/images/*.png'],
 	},
 	html: './src/*.html',
 	scripts: './src/*.js',
-	stylesheets: ['src/styles/*.css', 'src/styles/*.scss', 'src/styles/*.sass'],
+	stylesheets: ['src/*.css', 'src/*.scss', 'src/*.sass'],
+	images: ['./src/*.jpg', './src/*.png'],
 	dest: './build/',
+	imagedest: './build/images/'
 };
 
 // --- Build ---
-gulp.task('build:clean', (cb) => {
+gulp.task('build:clean', function(cb) {
 	return del(paths.dest, cb);
 });
 
-gulp.task('build:html', (done) => {
-	glob(paths.html, (err, files) => {
-		if (err) done(err);
-
-		/*gulp.src('src/*.html')
-			.pipe(injectCSS())
-			.pipe(gulp.dest(paths.dest));*/
-		  
-		const tasks = files.map(entry => {
-			return gulp.src('src/*.html')
-				.pipe(injectCSS())
-				.pipe(injectJS())
-				.pipe(gulp.dest('build'))
-				.pipe(gulp.dest(paths.dest));
-		});
-
-		es.merge(tasks).on('end', done);
-	});
+gulp.task('build:html', function (done) {
+	return gulp.src('src/*.html')
+		.pipe(injectCSS())
+		.pipe(injectJS())
+		.pipe(gulp.dest(paths.dest));
 });
 
-// Build experiment
-gulp.task('build', () => {
-	return sequence('build:clean', 'build:html')();
+gulp.task('build:images', function (done) {
+	return gulp.src(paths.images)
+		.pipe(webp(
+			imagemin(['src/images/*.{jpg,png}'], paths.imagedest, {
+				use: [
+					imageminWebp({quality: 70})
+				]
+			})))
+		.pipe(gulp.dest(paths.imagedest))
 });
 
-// Watch and rebuild when experiment files change
-gulp.task('watch', () => {
-	gulp.watch(paths.src.html, ['build']);
-	gulp.watch(paths.src.scripts, ['build']);
-	gulp.watch(paths.src.stylesheets, ['build']);
+// Build code
+gulp.task('build', gulp.series('build:clean', 'build:html', 'build:images'));
+
+// Watch and rebuild when files change
+gulp.task('watch', function () {
+	gulp.watch(paths.src.html, gulp.series('build'));
+	gulp.watch(paths.src.scripts, gulp.series('build'));
+	gulp.watch(paths.src.stylesheets, gulp.series('build'));
+	gulp.watch(paths.src.images, gulp.series('build'));
 });
 
 // Default task
-gulp.task('default', ['build', 'watch']);
+gulp.task('default', gulp.parallel('build', 'watch'));
