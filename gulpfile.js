c00e7b6199121b3b60d5e18685c9333597189f06
Butterfield <dave.butterfield@wwwinc.com>
2019-03-04 14:26:43 -0500

    FED Project Scaffold

--- gulpfile.js
+++ gulpfile.js
@@ -2,36 +2,45 @@
 const gulp = require('gulp');
 
 // Gulp helpers
-//const insert = require('gulp-insert');
-//const sequence = require('gulp-sequence');
-//const glob = require('glob');
-//const es = require('event-stream');
 const del = require('del');
 const injectCSS = require('gulp-inject-css');
 const injectJS = require('gulp-inject-js');
 const webp = require('gulp-webp');
 const imagemin = require('imagemin');
 const imageminWebp = require('imagemin-webp');
+// SCSS
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
 
 // Define relevant paths
 const paths = {
 	src: {
 		html: './src/**/*.html',
 		scripts: './src/**/*.js',
-		stylesheets: ['./src/**/*.css', './src/**/*.scss', './src/**/*.sass'],
+		styles: './src/styles/',
+		//stylesheets: ['./src/**/*.css', './src/**/*.scss', './src/**/*.sass'],
+		stylesheets: ['./src/**/*.scss'],
 		images: ['./src/images/*.jpg', './src/images/*.png'],
 	},
 	html: './src/*.html',
 	scripts: './src/*.js',
-	stylesheets: ['src/*.css', 'src/*.scss', 'src/*.sass'],
+	styles: './src/styles/',
+	//stylesheets: ['src/*.css', 'src/*.scss', 'src/*.sass'],
+	stylesheets: ['src/*.scss'],
 	images: ['./src/*.jpg', './src/*.png'],
 	dest: './build/',
 	imagedest: './build/images/'
 };
 
 // --- Build ---
-gulp.task('build:clean', function(cb) {
+gulp.task('build:clean', function (cb) {
 	return del(paths.dest, cb);
+});
+
+gulp.task('build:sass', function () {
+	return gulp.src('src/styles/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest(paths.styles))
 });
 
 gulp.task('build:html', function (done) {
@@ -46,14 +55,16 @@ gulp.task('build:images', function (done) {
 		.pipe(webp(
 			imagemin(['src/images/*.{jpg,png}'], paths.imagedest, {
 				use: [
-					imageminWebp({quality: 70})
+					imageminWebp({
+						quality: 70
+					})
 				]
 			})))
-		.pipe(gulp.dest(paths.imagedest))
+		.pipe(gulp.dest(paths.imagedest));
 });
 
 // Build code
-gulp.task('build', gulp.series('build:clean', 'build:html', 'build:images'));
+gulp.task('build', gulp.series('build:clean', 'build:sass', 'build:html', 'build:images'));
 
 // Watch and rebuild when files change
 gulp.task('watch', function () {
